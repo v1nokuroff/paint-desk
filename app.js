a83f0440ba5a3234856a3b3c96188b572be12301
Georgiy Mkrtychan <114615129+v1nokuroff@users.noreply.github.com>
2025-02-04 18:34:45 +0200

    Uploaded files

---
+++ app.js
@@ -0,0 +1,26 @@
+const board=document.querySelector('#board')
+const colors=['#ff4646','#014ea7','#49ff38','#ffde21','#9b13bd','#fda838','#ff70ff','#5dffc9']
+const SQUARES_NUNBER = 600
+
+for(let i=0;i<SQUARES_NUNBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    
+    square.addEventListener('mouseover', ()=>setColor(square))
+    square.addEventListener('mouseleave', ()=>removeColor(square))
+    board.append(square)
+}
+
+function setColor(element){
+    const color=getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`
+}
+function removeColor(element){
+    element.style.backgroundColor='#1d1d1d'
+    element.style.boxShadow=`0 0 2px #000`
+}
+function getRandomColor(){
+    const index= Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
